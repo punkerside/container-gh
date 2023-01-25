6915ab75c4e19ca2085202df693e0880794e789b
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-01-25 01:47:39 -0500

    agregando codigo

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine:3.17.1
+
+LABEL org.opencontainers.image.authors="punkerside"
+
+# instalando paquetes
+RUN apk update && apk upgrade && apk add --no-cache bash curl
+
+# instalando github-cli
+RUN echo "@community http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
+RUN apk add --no-cache github-cli@community
+
+COPY init.sh /root/init.sh
+CMD [ "/root/init.sh" ]
---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# github login
+echo ${GITHUB_TOKEN} | gh auth login --with-token
+
+# clonando repositorio
+gh repo clone punkerside/${GITHUB_REPO}
+
+cd ${GITHUB_REPO}/
+
+GITHUB_TAGS=$(gh release list)
+
+if [ "$(gh release list | grep Latest | awk '{print $9 " " $2}')" == " Latest" ]
+then
+  MAJOR_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f1)
+  MINOR_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f2)
+  PATCH_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f3)
+  PATCH_VERSION=$((PATCH_VERSION+1))
+  gh release create v${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION} --generate-notes
+else
+  gh release create v0.0.1 --generate-notes
+fi
